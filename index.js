9807e9d236ead438b56488feb1ea0cf07433a848
AbdullahHamadax <abdallahamada114@gmail.com>
2025-03-19 17:01:53 +0200

    feat: fix highlighting logic error

--- index.js
+++ index.js
@@ -7,90 +7,47 @@ let guestScoreboard = document.getElementById("guest-scoreboard");
 let gScore = 0;
 let hScore = 0;
 
+function updateWinner() {
+  homeScoreboard.classList.remove("winner-animation");
+  guestScoreboard.classList.remove("winner-animation");
+
+  if (hScore > gScore) {
+    homeScoreboard.classList.add("winner-animation");
+  } else if (hScore < gScore) {
+    guestScoreboard.classList.add("winner-animation");
+  }
+}
+
 function incrementHomeOnce() {
   hScore += 1;
   homeScore.textContent = hScore;
-  if (hScore > gScore) {
-    homeScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else if (hScore < gScore) {
-    guestScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else {
-    homeScoreboard.classList.remove("winner-animation");
-    guestScoreboard.classList.remove("winner-animation");
-  }
+  updateWinner();
 }
 function incrementHomeTwice() {
   hScore += 2;
   homeScore.textContent = hScore;
-  if (hScore > gScore) {
-    homeScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else if (hScore < gScore) {
-    guestScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else {
-    homeScoreboard.classList.remove("winner-animation");
-    guestScoreboard.classList.remove("winner-animation");
-  }
+  updateWinner();
 }
 function incrementHomeThrice() {
   hScore += 3;
   homeScore.textContent = hScore;
-  if (hScore > gScore) {
-    homeScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else if (hScore < gScore) {
-    guestScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else {
-    homeScoreboard.classList.remove("winner-animation");
-    guestScoreboard.classList.remove("winner-animation");
-  }
+  updateWinner();
 }
 
 function incrementGuestOnce() {
   gScore += 1;
   guestScore.textContent = gScore;
-  if (hScore > gScore) {
-    homeScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else if (hScore < gScore) {
-    guestScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else {
-    homeScoreboard.classList.remove("winner-animation");
-    guestScoreboard.classList.remove("winner-animation");
-  }
+  updateWinner();
 }
 function incrementGuestTwice() {
   gScore += 2;
   guestScore.textContent = gScore;
-  if (hScore > gScore) {
-    homeScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else if (hScore < gScore) {
-    guestScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else {
-    homeScoreboard.classList.remove("winner-animation");
-    guestScoreboard.classList.remove("winner-animation");
-  }
+  updateWinner();
 }
 function incrementGuestThrice() {
   gScore += 3;
   guestScore.textContent = gScore;
-  if (hScore > gScore) {
-    homeScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else if (hScore < gScore) {
-    guestScoreboard.classList.add("winner-animation");
-    console.log(homeScoreboard);
-  } else {
-    homeScoreboard.classList.remove("winner-animation");
-    guestScoreboard.classList.remove("winner-animation");
-  }
+  updateWinner();
 }
 
 function resetScore() {
